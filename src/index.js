c0e2e2224cf86376a7821bc2d2683f4829525b24
M <mykhailo.mykytas@c0ffee.tech>
2018-05-23 00:08:49 +0300

    init2

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by M on 22.05.2018.
+ */
+import exported from './some.js';
+console.log("yolo");
+console.log(exported(2,2))
---
+++ src/some.js
@@ -0,0 +1,12 @@
+/**
+ * Created by M on 22.05.2018.
+ */
+function sum(x, y) {
+    return x + y;
+}
+
+function exported(x, y) {
+    return sum(x , y);
+}
+
+export default exported;
